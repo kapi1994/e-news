c067d4850d00070a8b7e202acb044f94b2bf9079
kapi1994 <nemanjajovicic13@gmail.com>
2021-09-08 21:46:08 +0200

    Created new pages

--- pages/fixed/footer.php
+++ pages/fixed/footer.php
@@ -1,39 +1,3 @@
-<footer class="mt-auto bg-light">
-    <div class="container">
-        <div class="row">
-            <div class="col-10 mx-auto">
-                <?php
-                if (isset($_SESSION['users']) && $_SESSION['users']->roleName == "User") :
-                ?>
-                    <div class="row my-2">
-                        <div class="col-4">
-                            <h2>E-news</h2>
-                        </div>
-                        <div class="col-6 d-flex">
-                            <a href="data/logs.txt" class="nav-link">Dokumentacija</a>
-                            <a href="#" class="nav-link">Sitemap</a>
-                        </div>
-                    </div>
-                <?php endif; ?>
-            </div>
-        </div>
-        <div class="row pb-2">
-            <p class="text-center">All right reserved</p>
-        </div>
-
-    </div>
-
-</footer>
-<script src="assets/js/jquery.js"></script>
-
-<script src="assets/js/logAndReg.js"></script>
-
-
-<script src="assets/js/main.js"></script>
-
-
-
-<script src="assets/js/admin.js"></script>
 </body>
 
 </html>
--- pages/fixed/head.php
+++ pages/fixed/head.php
@@ -5,10 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Electronic portal</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <script src="assets/js/bootstrap.bundle.js"></script>
-    <script src="assets/js/jquery.js"></script>
+    <title>Document</title>
 </head>
 
-<body class="d-flex flex-column min-vh-100">
+<body>
