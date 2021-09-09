84baf805baf909c9f44d84ee9dc7c18186fcb403
kapi1994 <nemanjajovicic13@gmail.com>
2021-09-09 17:03:28 +0200

    added some changes

--- pages/pages/admin/categories/index.php
+++ pages/pages/admin/categories/index.php
@@ -1,6 +1,15 @@
 <section>
     <div class="container">
-        <div class="row my-5">
+        <div class="row my-3">
+            <div class="flex-start">
+                <div class="col-lg-3">
+                    <div class="d-grid">
+                        <a href="index.php?page=category_action" class="btn btn-primary">Add new category</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-3">
             <div class="col-xs-12">
                 <div class="table-responsive-sm table-responsive-md">
                     <table class="table text-center align-middle">
@@ -16,17 +25,19 @@
                         </thead>
                         <tbody>
                             <?php
-                            $categories = $conn->query("SELECT * FROM categories")->fetchAll();
+                            $categories = getAll('categories');
                             $rb = 1;
                             foreach ($categories as $category) :
+
+                                // foreach ($categories as $category) :
                             ?>
                                 <tr>
                                     <th scope="row"><?= $rb++ ?></th>
                                     <td><?= $category->name ?></td>
                                     <td><?= date("H:i:s d/m/Y", strtotime($category->created_at)) ?></td>
                                     <td><?= $category->updated_at != null ? date("H:i:s d/m/Y", strtotime($category->updated_at)) : "/" ?></td>
-                                    <td><a href="#" class="btn btn-success btn-sm">Update</a></td>
-                                    <td><button type="" class="btn btn-sm btn-danger" data-id="<?= $category->id ?>">Delete</button></td>
+                                    <td><a href="index.php?page=category_action&id=<?= $category->id ?>" class="btn btn-success btn-sm">Update</a></td>
+                                    <td><button type="button" class="btn btn-sm btn-danger " data-id="<?= $category->id ?>">Delete</button></td>
                                 </tr>
                             <?php endforeach; ?>
                         </tbody>
---
+++ pages/pages/admin/categories/insert_update_category.php
@@ -0,0 +1,23 @@
+<section>
+    <?php
+    if (isset($_GET['id'])) {
+        $category = $_GET['id'];
+        $category_data = getOneFetchAndCheckData("categories", 'id', $_GET['id'], "fetch");
+    }
+    ?>
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-sm-8 col-lg-6 mx-auto">
+                <form method="POST">
+                    <input type="hidden" name="categoryId" id="categoryId" value="<?= isset($_GET['id']) ? $category_data->id : '' ?>">
+                    <div class="mb-3">
+                        <label for="" class="mb-1">Name:</label>
+                        <input type="text" name="categoryName" id="categoryName" class="form-control" value="<?= isset($_GET['id']) ? $category_data->name : '' ?>">
+                        <em id="categoryNameErrorMessage"></em>
+                    </div>
+                    <div class="d-grid"><button type="button" class="btn btn-primary" id="btnCategory">Save</button></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
