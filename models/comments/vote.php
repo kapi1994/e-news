166e11b8b122878402c669fa5c4605f6a2e27491
kapi1994 <nemanjajovicic13@gmail.com>
2021-09-26 03:48:14 +0200

    added voting functionality

--- models/comments/vote.php
+++ models/comments/vote.php
@@ -1,20 +1,38 @@
 <?php
+session_start();
 header("Content-type:application/json");
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     require_once '../../config/connection.php';
     require_once '../function.php';
 
     $action = $_POST['action'];
-    // $post = $_POST['post'];
+
     $comment = $_POST['comment'];
     $user_id  = isset($_SESSION['user'])  ? $_SESSION['user']->id : '';
+    // echo json_encode($action);
 
-    if ($action == 'like') {
-        $query = $conn->query("SELECT * FROM reactions WHERE user_id = '$user_id'  AND comment_id = '$comment'");
-        if ($query->rowCount() == 0) {
-        } else {
+    define("REACT_ON", 1);
+    define("REACT_OFF", 0);
+    // $query = getReactions($user_id, $comment, 'rowCount');
+    // echo json_encode(getReactions($user_id, $comment, 'rowCount'));
+    if (getReactions($user_id, $comment, 'rowCount') == 0) {
+        // echo json_encode('insert');
+        try {
+            insertVote($action, $comment, $user_id, REACT_ON);
+            echo json_encode("reaction inserted");
+            http_response_code(201);
+        } catch (PDOException $th) {
+            echo json_encode($th->getMessage());
+            http_response_code(500);
         }
     } else {
+        try {
+            updateStatusVote($user_id, $comment, $action, REACT_ON, REACT_OFF);
+            http_response_code(204);
+        } catch (PDOException $th) {
+            echo json_encode($th->getMessage());
+            http_response_code(500);
+        }
     }
 } else {
     http_response_code(404);
