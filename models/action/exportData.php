22b078f58aef4e07762e51bc14831b419fb72c73
kapi1994 <nemanjajovicic13@gmail.com>
2021-10-02 01:38:22 +0200

    Added funcionality to export data as excel file

---
+++ models/action/exportData.php
@@ -0,0 +1,20 @@
+<?php
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+    require_once '../../config/connection.php';
+    require_once '../function.php';
+    if ($action == 'excel') {
+        $field = getColumnsTable();
+        $data = getAll('categories');
+        $timestamp = time();
+        $filename = 'categoriesNJ' . $timestamp . '.xls';
+        header("Content-Type: application/vnd.ms-excel");
+        header("Content-Disposition: attachment; filename=\"$filename\"");
+        foreach ($field as $d) echo $d->COLUMN_NAME . "\t";
+        echo "\n";
+        foreach ($data as $d) echo $d->id . "\t" . $d->name . "\t" . $d->created_at . "\t" . $d->updated_at . "\t\n";
+        exit();
+    } else {
+        echo 'json';
+    }
+}
