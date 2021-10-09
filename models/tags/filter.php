b86098530f7d2ee676f401bea1831b852ffa51bf
kapi1994 <nemanjajovicic13@gmail.com>
2021-10-09 23:57:39 +0200

    added pagination with filters to tags

--- models/tags/filter.php
+++ models/tags/filter.php
@@ -4,24 +4,50 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $text = $_GET['text'];
     $order = $_GET['order'];
     $compareString = trim("%$text%");
+    $pagination = $_GET['limit'];
     require_once '../../config/connection.php';
-    $baseQuery =  "SELECT * FROM tags ";
+    $query  = '';
+    $baseQueryA = "SELECT COUNT(*) as numOfTags FROM tags";
+    $baseQueryB =  "SELECT * FROM tags ";
     if ($text) {
         // echo json_encode('filtriranje');
-        $baseQuery .= "WHERE name LIKE '$compareString'";
+        $query .= "WHERE name LIKE '$compareString'";
         // echo json_encode($baseQuery);
     }
     if ($order) {
         if ($order == 0) {
-            $baseQuery .= " ORDER BY created_at DESC";
+            $query .= " ORDER BY created_at DESC";
         } else {
-            $baseQuery .= " ORDER BY created_at ASC";
+            $query .= " ORDER BY created_at ASC";
         }
     } else {
-        $baseQuery .= " ORDER BY created_at DESC";
+        $query .= " ORDER BY created_at DESC";
     }
-    $query = $conn->query($baseQuery)->fetchAll();
-    echo json_encode($query);
+    define("OFFSET", 5);
+    // $offse
+    $baseQueryA = $baseQueryA . $query;
+    if ($pagination && $pagination > 0) {
+        $limit = ((int)$_GET['limit']) * OFFSET;
+        $offset = OFFSET;
+        $query .= " LIMIT $limit, $offset";
+    } else if ($pagination == 0 || !$pagination) {
+        $limit = 0;
+        $offset = OFFSET;
+        $query .= " LIMIT $limit, $offset";
+    }
+
+    // $query = $conn->query($baseQuery)->fetchAll();
+    // echo json_encode($query);
+
+    $baseQueryB = $baseQueryB . $query;
+    $res = $conn->query($baseQueryA)->fetch();
+    $elements = $conn->query($baseQueryB)->fetchAll();
+    $numOfPages = ceil($res->numOfTags / $offset);
+    echo json_encode([
+        'tags' => $elements,
+        'pages' => $numOfPages,
+        'limit' => $pagination
+    ]);
 } else {
     http_response_code(404);
 }
