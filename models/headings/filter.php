7f3648beddbebd23b5fed4eb27146ea10b263005
kapi1994 <nemanjajovicic13@gmail.com>
2021-10-09 23:58:09 +0200

    added pagination with filters to headings

--- models/headings/filter.php
+++ models/headings/filter.php
@@ -1,27 +1,52 @@
 <?php
 header("Content-type:application/json");
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+    $query = '';
+
     $text = $_GET['text'];
     $compareString = trim("%$text%");
     $order = $_GET['order'];
+    $pagination = $_GET['limit'];
+    define("OFFSET", 5);
+    $offset = OFFSET;
     require_once '../../config/connection.php';
-    $baseQuery =  "SELECT h.*, c.name as categoryName FROM headings h INNER JOIN categories c ON h.category_id = c.id ";
+    require_once '../function.php';
+    $baseQueryA = "SELECT COUNT(*) as headingElementsCount FROM headings h JOIN categories c ON h.category_id = c.id";
+    $baseQueryB = "SELECT h.*, c.name as categoryName FROM headings h JOIN categories c ON h.category_id = c.id";
     if ($text) {
-        // echo json_encode('filtriranje');
-        $baseQuery .= "WHERE h.name LIKE '$compareString'";
-        // echo json_encode($baseQuery);
+        $query .= "WHERE h.name LIKE '$compareString'";
     }
     if ($order) {
         if ($order == 0) {
-            $baseQuery .= " ORDER BY h.created_at DESC";
+            $query .= " ORDER BY h.created_at DESC";
         } else {
-            $baseQuery .= " ORDER BY h.created_at ASC";
+            $query .= " ORDER BY h.created_at ASC";
         }
     } else {
-        $baseQuery .= " ORDER BY h.created_at DESC";
+        $query .= " ORDER BY h.created_at DESC";
     }
-    $query = $conn->query($baseQuery)->fetchAll();
-    echo json_encode($query);
+    $baseQueryA = $baseQueryA . $query;
+    if ($pagination && $pagination > 0) {
+        $limit = ((int)$_GET['limit']) * OFFSET;
+        $offset = OFFSET;
+        $query .= " LIMIT $limit, $offset";
+    } else if ($pagination == 0 || !$pagination) {
+        $limit = 0;
+        $offset = OFFSET;
+        $query .= " LIMIT $limit, $offset";
+    }
+
+    $baseQueryB = $baseQueryB . $query;
+    // echo json_encode($baseQueryA);
+    $rez = $conn->query($baseQueryB)->fetchAll();
+    $res = $conn->query($baseQueryA)->fetch();
+    $numOfPages = ceil($res->headingElementsCount / OFFSET);
+    echo json_encode([
+        'headings' => $rez,
+        'pages' => $numOfPages,
+        'limit' => $pagination
+    ]);
 } else {
     http_response_code(404);
 }
