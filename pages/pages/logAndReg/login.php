796daf3bc6c1609720a0ac1d6e9226e891ec1b1e
kapi1994 <nemanjajovicic13@gmail.com>
2021-10-10 02:37:09 +0200

    displaying error messages from login

--- pages/pages/logAndReg/login.php
+++ pages/pages/logAndReg/login.php
@@ -2,7 +2,22 @@
     <div class="container">
         <div class="row my-5">
             <div class="col-lg-6 mx-auto">
+                <ul class="list-group">
+                    <?php
 
+                    if (isset($_SESSION['errors'])) :
+                        $errors = $_SESSION['errors'];
+                        foreach ($errors as $error) :
+                    ?>
+                            <div class="alert alert-danger fw-bolder" role="alert">
+                                <?= $error ?>
+                            </div>
+                    <?php
+                        endforeach;
+                    endif;
+                    session_destroy();
+                    ?>
+                </ul>
 
                 <form action="models/action/login.php" method="POST">
                     <div class="mb-3">
