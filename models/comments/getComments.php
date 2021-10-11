e7ac5dc03fbb530f3f8b85254e452bee07bd1b58
kapi1994 <nemanjajovicic13@gmail.com>
2021-10-11 02:11:24 +0200

    Fixed returning values

--- models/comments/getComments.php
+++ models/comments/getComments.php
@@ -1,12 +1,17 @@
 <?php
+session_start();
 header("Content-type:application/json");
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $post = $_POST['post_id'];
     $parent_comment = isset($_POST['comments']) ? $_POST['comments'] : 0;
     require_once '../../config/connection.php';
     require_once '../function.php';
-
+    $user = isset($_SESSION['user']) ? $_SESSION['user'] : '';
     $comments = getCommentsWithReaction($post, $parent_comment);
+    echo json_encode([
+        'comments' => $comments,
+        'user' => $user
+    ]);
 } else {
     http_response_code(404);
 }
