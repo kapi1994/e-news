d97084f749469b4dbec9193164fb19c4e4ca219f
kapi1994 <nemanjajovicic13@gmail.com>
2021-10-11 19:26:08 +0200

    Clean unnecessary code

--- models/headings/filter.php
+++ models/headings/filter.php
@@ -2,52 +2,27 @@
 header("Content-type:application/json");
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
-    $query = '';
+    // $query = '';
 
     $text = $_GET['text'];
     $compareString = trim("%$text%");
     $order = $_GET['order'];
-    $pagination = $_GET['limit'];
-    define("OFFSET", 5);
-    $offset = OFFSET;
+    $limit = isset($_GET['limit']) ? $_GET['limit'] : 0;
+
+
     require_once '../../config/connection.php';
     require_once '../function.php';
-    // $query = ''
-    $baseQueryA = "SELECT COUNT(*) as headingElementsCount FROM headings h JOIN categories c ON h.category_id = c.id";
-    $baseQueryB = "SELECT h.*, c.name as categoryName FROM headings h JOIN categories c ON h.category_id = c.id";
-    if ($text) {
-        $query .= "WHERE h.name LIKE '$compareString'";
-    }
-    if ($order) {
-        if ($order == 0) {
-            $query .= " ORDER BY h.created_at DESC";
-        } else {
-            $query .= " ORDER BY h.created_at ASC";
-        }
-    } else {
-        $query .= " ORDER BY h.created_at DESC";
-    }
-    $baseQueryA = $baseQueryA . $query;
-    if ($pagination && $pagination > 0) {
-        $limit = ((int)$_GET['limit']) * OFFSET;
-        $offset = OFFSET;
-        $query .= " LIMIT $limit, $offset";
-    } else if ($pagination == 0 || !$pagination) {
-        $limit = 0;
-        $offset = OFFSET;
-        $query .= " LIMIT $limit, $offset";
-    }
 
-    $baseQueryB = $baseQueryB . $query;
-    // echo json_encode($baseQueryA);
-    $rez = $conn->query($baseQueryB)->fetchAll();
-    $res = $conn->query($baseQueryA)->fetch();
-    $numOfPages = ceil($res->headingElementsCount / OFFSET);
+    $res = getHeadingWithCategory($limit, $compareString, $order);
+    $pages  = getNumOfHeadings($compareString, 'pagination');
+
+
     echo json_encode([
-        'headings' => $rez,
-        'pages' => $numOfPages,
-        'limit' => $pagination
+        'headings' => $res,
+        'pages' => $pages,
+        'limit' => $limit
     ]);
+    http_response_code(200);
 } else {
     http_response_code(404);
 }
