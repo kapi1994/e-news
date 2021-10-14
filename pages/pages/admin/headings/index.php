6c4d69d4890a92bad9089c39992a4a806d684e92
kapi1994 <nemanjajovicic13@gmail.com>
2021-10-14 21:29:45 +0200

    Checking added if headings have more than 0 elements

--- pages/pages/admin/headings/index.php
+++ pages/pages/admin/headings/index.php
@@ -29,83 +29,88 @@ if (isset($_SESSION['user'])) {
                         </select>
                     </div>
                 </div>
-                <div class="col-lg-3 mb-3">
-                    <div class="d-grid">
-                        <button class="btn btn-primary d-lg-none d-mb-2" data-bs-toggle="modal" data-bs-target="" ">Filters</button>
-                    </div>
-                </div>
-            
+
             </div>
             <div class=" col-lg-9 col-xs-12">
-                            <div id="headingResponseMessages"></div>
-                            <div class="table-responsive-sm table-responsive-md">
-                                <table class="table text-center align-middle">
-                                    <thead class="table-dark">
-                                        <tr>
-                                            <th scope="col">#</th>
-                                            <th scope="col">Name</th>
-                                            <th scope="col">Category name</th>
-                                            <th scope="col">Created at</th>
-                                            <th scope="col">Updated at</th>
-                                            <th scope="col">Update</th>
-                                            <th scope="col">Delete</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody id="headings">
-                                        <?php
-                                        $rb  = 1;
-                                        // var_dump(getNumOfRows());
-                                        $headings = getHeadingWithCategory('fetch');
-                                        foreach ($headings as $heading) :
-                                        ?>
-                                            <tr>
-                                                <th scope="row"><?= $rb++ ?></th>
-                                                <td><?= $heading->name ?></td>
-                                                <td><?= $heading->categoryName ?></td>
-                                                <td><?= date("H:i:s d/m/Y", strtotime($heading->created_at)) ?></td>
-                                                <td><?= $heading->updated_at != null ? date("H:i:s d/m/Y") : "/" ?></td>
-                                                <td><a href="index.php?page=heading_action&id=<?= $heading->id ?>" class="btn btn-success btn-sm">Update</a></td>
-                                                <td><button class="btn btn-danger btn-sm delete-heading" data-id="<?= $heading->id ?>">Delete</button></td>
-                                            </tr>
-                                        <?php endforeach; ?>
-                                    </tbody>
-                                </table>
+                <div id="headingResponseMessages"></div>
+                <div class="table-responsive-sm table-responsive-md">
+                    <table class="table text-center align-middle">
+                        <thead class="table-dark">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Category name</th>
+                                <th scope="col">Created at</th>
+                                <th scope="col">Updated at</th>
+                                <th scope="col">Update</th>
+                                <th scope="col">Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody id="headings">
+                            <?php
+                            $headingsCount = getNumOfHeadings('count');
+                            if ($headingsCount->numOfHeadings > 0) :
+                            ?>
                                 <?php
-                                $headingElementsCount =  getNumOfHeadings('', 'count');
-                                // var_dump($headingElementsCount);
-                                if ($headingElementsCount->numOfHeadings > 5) :
+                                $rb  = 1;
+                                // var_dump(getNumOfRows());
+                                $headings = getHeadingWithCategory();
 
+                                foreach ($headings as $heading) :
                                 ?>
-                                    <div class="row mt-3">
-                                        <div class="col-4 mx-auto">
-                                            <nav aria-label="...">
-                                                <ul class="pagination" id="headingPagination">
-                                                    <?php
-                                                    $headingPagination = getNumOfHeadings('', 'fetch');
-                                                    for ($i = 0; $i < $headingPagination; $i++) :
-                                                        if ($i == 0) :
-                                                    ?>
-                                                            <li class="page-item active"><a class="page-link heading-pagination" href="#" data-limit="<?= $i ?>"><?= ($i + 1) ?></a></li>
-                                                        <?php
-                                                        endif;
-                                                        if ($i != 0) :
-                                                        ?>
-                                                            <li class="page-item"><a class="page-link heading-pagination" href="#" data-limit="<?= $i ?>"><?= ($i + 1) ?></a></li>
-                                                    <?php
-                                                        endif;
-                                                    endfor;
-                                                    ?>
+                                    <tr>
+                                        <th scope="row"><?= $rb++ ?></th>
+                                        <td><?= $heading->name ?></td>
+                                        <td><?= $heading->categoryName ?></td>
+                                        <td><?= date("H:i:s d/m/Y", strtotime($heading->created_at)) ?></td>
+                                        <td><?= $heading->updated_at != null ? date("H:i:s d/m/Y") : "/" ?></td>
+                                        <td><a href="index.php?page=heading_action&id=<?= $heading->id ?>" class="btn btn-success btn-sm">Update</a></td>
+                                        <td><button class="btn btn-danger btn-sm delete-heading" data-id="<?= $heading->id ?>">Delete</button></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            <?php else : ?>
+                                <tr>
+                                    <th colspan="7" class="text-center">We dont have any headings at the moment
+                                </tr>
+                            <?php endif; ?>
+                        </tbody>
+                    </table>
+                    <?php
+                    $headingElementsCount =  getNumOfHeadings('count');
 
-                                                </ul>
-                                            </nav>
-                                        </div>
-                                    </div>
+                    if ($headingElementsCount->numOfHeadings > 5) :
 
-                                <?php
+                    ?>
+                        <div class="row mt-3">
+                            <div class="col-4 mx-auto">
+                                <nav aria-label="...">
+                                    <ul class="pagination" id="headingPagination">
+                                        <?php
+                                        $headingPagination = getNumOfHeadings('fetch');
+                                        for ($i = 0; $i < $headingPagination; $i++) :
+                                            if ($i == 0) :
+                                        ?>
+                                                <li class="page-item active"><a class="page-link heading-pagination" href="#" data-limit="<?= $i ?>"><?= ($i + 1) ?></a></li>
+                                            <?php
+                                            endif;
+                                            if ($i != 0) :
+                                            ?>
+                                                <li class="page-item"><a class="page-link heading-pagination" href="#" data-limit="<?= $i ?>"><?= ($i + 1) ?></a></li>
+                                        <?php
+                                            endif;
+                                        endfor;
+                                        ?>
 
-                                endif; ?>
+                                    </ul>
+                                </nav>
                             </div>
-                    </div>
+                        </div>
+
+                    <?php
+
+                    endif; ?>
                 </div>
             </div>
+        </div>
+    </div>
 </section>
