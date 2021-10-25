bb622157d38c2f8a4cb7229e8833faf2a81523a2
kapi1994 <nemanjajovicic13@gmail.com>
2021-10-25 23:35:09 +0200

    Added some classes for styling

--- pages/pages/user/author.php
+++ pages/pages/user/author.php
@@ -2,15 +2,15 @@
     <div class="row my-3">
         <div class="col-lg-10 mx-auto">
             <div class="row">
-                <div class="col-lg-5">
-                    <img src="assets/images/author/a.jpg" alt="" class="img-fluid">
+                <div class="col-sm-8 col-md-8 mx-auto col-lg-5">
+                    <img src="assets/images/author/a.jpg" alt="" class="img-thumbnail mx-auto d-block">
                 </div>
                 <div class="col-lg-7">
                     <h1 class="text-center my-5"> Nemanja Jovičić</h1>
-                    <p class="text-muted text-justify fs-4">Strastveni ljubitelj video igara.</p>
-                    <div class="col-lg-5">
+                    <p class="text-muted text-center fs-4">Strastveni ljubitelj video igara.</p>
+                    <div class="col-sm-6 col-md-6 mx-auto col-lg-5">
                         <div class="d-grid">
-                            <a href="#" class="btn btn-outline-primary mt-2">Portfolio</a>
+                            <a href="models/action/exportData.php?action=word" class="btn btn-outline-primary mt-2">Portfolio</a>
                         </div>
                     </div>
                 </div>
