8539729e1d03627526a0b39c8314e217eb5d412a
kapi1994 <nemanjajovicic13@gmail.com>
2021-10-28 21:54:42 +0200

    pages that are available for user role are separated from admin and journalist role

--- index.php
+++ index.php
@@ -8,57 +8,11 @@ $page = '';
 if (isset($_GET['page'])) {
     $page =  $_GET['page'];
     switch ($page) {
-        case 'categories':
-            include 'pages/pages/admin/categories/index.php';
-            break;
-        case 'category_action':
-            include 'pages/pages/admin/categories/insert_update_category.php';
-            break;
-
-        case 'headings':
-            include "pages/pages/admin/headings/index.php";
-            break;
-        case 'heading_action':
-            include 'pages/pages/admin/headings/insert_update_heading.php';
-            break;
-
-        case 'tags':
-            include 'pages/pages/admin/tags/index.php';
-            break;
-        case 'tag_action':
-            include 'pages/pages/admin/tags/insert_update_category.php';
-            break;
-
-
-        case 'posts':
-            include 'pages/pages/admin/posts/index.php';
-            break;
-        case 'post_action':
-            include 'pages/pages/admin/posts/insert_update_post.php';
-            break;
-        case 'post_details':
-            include 'pages/pages/admin/posts/post_details.php';
-            break;
-        case 'users':
-            include 'pages/pages/admin/users/index.php';
-            break;
-        case 'user_action':
-            include 'pages/pages/admin/users/insert_update_action_user.php';
-            break;
 
         case 'home':
             include 'pages/pages/user/index.php';
             break;
 
-        case 'admin_home':
-            include 'pages/pages/admin/index.php';
-            break;
-        case 'tasks':
-            include 'pages/pages/admin/tasks/index.php';
-            break;
-        case 'insert_update_task':
-            include 'pages/pages/admin/tasks/insert_update.php';
-            break;
         case 'login':
             include 'pages/pages/logAndReg/login.php';
             break;
@@ -72,6 +26,14 @@ if (isset($_GET['page'])) {
         case 'singleNews':
             include 'pages/pages/user/singleNews.php';
             break;
+
+        case 'author':
+            include 'pages/pages/user/author.php';
+            break;
+        default:
+            include 'pages/fixed/status.php';
     }
+} else {
+    include 'pages/pages/user/index.php';
 }
 include 'pages/fixed/footer.php';
