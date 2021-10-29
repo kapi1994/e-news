2a1b44fc364cdac507ab0056f1c425103ae0a45d
kapi1994 <nemanjajovicic13@gmail.com>
2021-10-29 23:50:24 +0200

    FIle is removed

--- pages/fixed/403.php
+++
@@ -1,9 +0,0 @@
-<section>
-    <div class="container">
-        <div class="row my-5">
-            <div class="col-lg-3 mx-auto">
-                <h1></h1>
-            </div>
-        </div>
-    </div>
-</section>
