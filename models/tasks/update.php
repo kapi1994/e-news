64583e04449358daffa9ebae8b7122d33c57f009
kapi1994 <nemanjajovicic13@gmail.com>
2021-10-30 00:17:32 +0200

    Added validation and update for the task

--- models/tasks/update.php
+++ models/tasks/update.php
@@ -2,11 +2,30 @@
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $id = $_POST['id'];
     $user_id = $_POST['user_id'];
-    $desc = isset($_POST['desc']) ? $_POST['desc'] : '';
-    if ($desc) {
-        echo json_encode("desc");
+    $desc = $_POST['description'];
+
+    $errors = [];
+    if ($desc == '') {
+        array_push($errors, "Descritpion must't be empty");
+    }
+    if ($desc == 0) {
+        array_push($errors, "User must be chossen");
+    }
+    if (count($errors) > 0) {
+        foreach ($errors as $error) {
+            echo json_encode($err);
+            http_response_code(422);
+        }
     } else {
-        echo json_encode($id);
+        try {
+            require_once '../../config/connection.php';
+            require_once '../function.php';
+            updateTask($desc, $user_id, $id);
+            http_response_code(204);
+        } catch (PDOException $th) {
+            echo json_encode($th->getMessage());
+            http_response_code(500);
+        }
     }
 } else {
     http_response_code(404);
