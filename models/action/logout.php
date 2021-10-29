af1ee9c2138d3beaa2ab4e6b5b0b54f5236c1c25
kapi1994 <nemanjajovicic13@gmail.com>
2021-10-29 23:48:11 +0200

    Added funcionality to track when someone is logout into log file

--- models/action/logout.php
+++ models/action/logout.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 if (isset($_SESSION['user'])) {
+    require_once '../function.php';
+    insertActivity("Logout", $_SESSION['user']->email);
     unset($_SESSION['user']);
     header("Location:../../index.php?page=home");
 }
