2d3bf444fa108a9db63e01d474bcc2ab9295daab
kapi1994 <nemanjajovicic13@gmail.com>
2021-11-17 15:39:44 +0100

    Added file for getting vote for certain comment

--- models/comments/getVote.php
+++ models/comments/getVote.php
@@ -4,11 +4,18 @@ header("Content-type:application/json");
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $comment_id = $_GET['comment_id'];
     $user_id = isset($_SESSION['user']) ? $_SESSION['user']->id : "";
-
+    $action = isset($_GET['action']);
     require_once '../../config/connection.php';
     require_once '../function.php';
     $res = getVote($comment_id, $user_id);
-    echo json_encode($res);
+    $countLikeVotes = countVote($comment_id, 'likes', 'likeCount');
+    $countDisslikeVotes = countVote($comment_id, 'disslikes', 'disslikeCount');
+    echo json_encode([
+        'vote' => $res,
+        'user_id' => $user_id,
+        'likeCount' => $countLikeVotes,
+        'disslikeCount' => $countDisslikeVotes
+    ]);
 } else {
     http_response_code(404);
 }
