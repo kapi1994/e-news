92db8c11da91beb07d1f1bbf83b3b2e038f9c294
kapi1994 <nemanjajovicic13@gmail.com>
2021-11-20 22:51:52 +0100

    Added new parameter to the function

--- models/comments/getComments.php
+++ models/comments/getComments.php
@@ -4,10 +4,11 @@ header("Content-type:application/json");
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
     $post = $_GET['post'];
     $parent_comment = isset($_GET['comment']) ? $_GET['comment'] : 0;
+    $user_id = $_GET['user'];
     require_once '../../config/connection.php';
     require_once '../function.php';
     $user = isset($_SESSION['user']) ? $_SESSION['user'] : '';
-    $comments = getCommentsWithReaction($post, $parent_comment);
+    $comments = getCommentsWithReaction($post, $user_id, $parent_comment);
     echo json_encode([
         'comments' => $comments,
         'user' => $user
