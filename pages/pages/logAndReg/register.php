abb1181e60da3085cbec713ea2c301fc126271d6
kapi1994 <nemanjajovicic13@gmail.com>
2021-11-21 03:47:05 +0100

    Added redirection link to register page, if user already have account

--- pages/pages/logAndReg/register.php
+++ pages/pages/logAndReg/register.php
@@ -4,27 +4,28 @@
             <div class="col-6 mx-auto">
                 <form action="">
                     <div class="mb-3">
-                        <label for="">First name:</label>
+                        <label for="userFirstName">First name:</label>
                         <input type="text" name="userFirstName" id="userFirstName" class="form-control">
                         <em id="userFirstNameErrorMessage"></em>
                     </div>
                     <div class="mb-3">
-                        <label for="">Last name:</label>
+                        <label for="userLastName">Last name:</label>
                         <input type="text" name="userLastName" id="userLastName" class="form-control">
                         <em id="userLastNameErrorMessage"></em>
                     </div>
                     <div class="mb-3">
-                        <label for="">Email:</label>
+                        <label for="iserEmail">Email:</label>
                         <input type="email" name="userEmail" id="userEmail" class="form-control">
                         <em id="userEmailErrorMessage"></em>
                     </div>
                     <div class="mb-3">
-                        <label for="">Password:</label>
+                        <label for="userPassword">Password:</label>
                         <input type="password" name="userPassword" id="userPassword" class="form-control">
                         <em id="userPasswordErrorMessage"></em>
                     </div>
-                    <div class="d-grid">
+                    <div class="d-grid gap-2">
                         <button class="btn btn-primary" id="btnRegister" type="button">Registrujte se</button>
+                        <a href="index.php?page=login" class="btn btn-outline-dark">Image nalog? Ulogujte se.</a>
                     </div>
                 </form>
             </div>
