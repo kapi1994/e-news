3bcd75b33b0050057605548698af49730189d1be
kapi1994 <nemanjajovicic13@gmail.com>
2021-11-24 00:34:27 +0100

    Added and removed  classes to news card

--- pages/pages/user/news.php
+++ pages/pages/user/news.php
@@ -42,14 +42,14 @@
             foreach ($posts as $post) :
 
             ?>
-                <div class="col-lg-3 my-2">
-                    <div class="card">
-                        <img src="assets/images/posts/normal/<?= $post->image_path ?>" class="card-img-top" alt="<?= $post->name ?>">
-                        <div class="card-body">
-                            <a href="index.php?page=singleNews&id=<?= $post->id ?>" class="nav-link text-dark">
-                                <h5 class="card-title fs-6"><?= $post->name ?></h5>
-                            </a>
-                        </div>
+                <div class="col-lg-3 ">
+                    <div class="card box h-100 d-flex flex-column">
+                        <img src="assets/images/posts/normal/<?= $post->image_path ?>" class="img-fluid" alt="<?= $post->name ?>">
+
+                        <a href="index.php?page=singleNews&id=<?= $post->id ?>" class="nav-link text-dark mt-auto">
+                            <h5 class="card-title fs-6"><?= $post->name ?></h5>
+                        </a>
+
                     </div>
                 </div>
             <?php endforeach; ?>
