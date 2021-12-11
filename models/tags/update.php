9e3200658f7c505f3fb4d8c2494fc7299348d207
kapi1994 <nemanjajovicic13@gmail.com>
2021-12-11 21:15:28 +0100

    Added new parameter to update function

--- models/tags/update.php
+++ models/tags/update.php
@@ -2,6 +2,7 @@
 header("Content-type:application/json");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $reName = '/^([A-Z]{1,}|[A-Z][a-z]{2,15}|[\w\d]{1,})(\s[A-Z]{1,}|\s[A-Z][a-z]{2,15}|\s[a-z]{2,})*$/';
+    $heading = $_POST['heading_id'];
     $errors = [];
     if (!preg_match($reName, $_POST['id'])) {
         array_push($errors, "Tag name isn't ok");
@@ -23,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 http_response_code(409);
             } else {
                 try {
-                    updateTag($_POST['name'], $date, $_POST['id']);
+                    updateTag($_POST['name'], $date, $heading, $_POST['id']);
                     http_response_code(204);
                 } catch (PDOException $th) {
                     echo json_encode($th->getMessage());
@@ -32,7 +33,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
         } else {
             try {
-                updateTag($_POST['name'], $date, $_POST['id']);
+                updateTag($_POST['name'], $date, $heading, $_POST['id']);
                 http_response_code(204);
             } catch (PDOException $th) {
                 echo json_encode($th->getMessage());
