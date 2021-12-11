5e33a3b110e15babdb1b0ede589c58cb92f0d261
kapi1994 <nemanjajovicic13@gmail.com>
2021-12-11 21:13:05 +0100

    Change order of parameters

--- models/users/filter.php
+++ models/users/filter.php
@@ -4,12 +4,13 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
     $text = $_GET['text'];
     $compareString = trim("%$text%");
     $order = $_GET['order'] ? $_GET['order'] : 0;
-    $role = $_GET['role'];
+    $role = $_GET['role'] ? $_GET['role'] : 0;
+    // echo json_Encode($role);
     $limit = isset($_GET['limit']) ? $_GET['limit'] : 0;
 
     require_once '../../config/connection.php';
     require_once '../function.php';
-    $usersPagination = userPagination($limit, $compareString, $role, $order);
+    $usersPagination = userPagination($limit, $compareString, $order, $role);
     $userPages = getNumOfUsers('pagination', $compareString, $role);
     echo json_encode([
         'res' => $usersPagination,
