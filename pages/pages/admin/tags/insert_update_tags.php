bb335abc9779a7cd95833c3e79281e22844bb431
kapi1994 <nemanjajovicic13@gmail.com>
2021-12-11 21:32:58 +0100

    Added new functionality for tags, to which section they belong

--- pages/pages/admin/tags/insert_update_tags.php
+++ pages/pages/admin/tags/insert_update_tags.php
@@ -6,8 +6,10 @@ if (isset($_SESSION['user'])) {
 } else {
     header("Location:admin.php?page=status&code=401");
 }
+
 ?>
 <section>
+
     <?php
     if (isset($_GET['id'])) {
         $tag = getOneFetchAndCheckData('tags', 'id', $_GET['id'], 'fetch');
@@ -25,6 +27,25 @@ if (isset($_SESSION['user'])) {
                         <label for="" class="mb-1">Name:</label>
                         <input type="text" name="tagName" id="tagName" class="form-control" value="<?= isset($_GET['id']) ? $tag->name : '' ?>">
                         <em id="tagNameErrorMessage"></em>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="">Heading:</label>
+                        <select name="heading_tag" id="heading_tag" class="form-select">
+                            <option value="0">Choose</option>
+                            <?php
+                            if ($_SESSION['user']->roleName == "Journalist") {
+                                $category_id = $_SESSION['user']->category_id;
+                                $headings = getHeadingByCategoryId($category_id);
+                            } else {
+                                $headings = getAll('headings');
+                            }
+                            foreach ($headings as $heading) :
+                            ?>
+                                <option value="<?= $heading->id ?>" <?php if (isset($_GET['id']) && $tag->heading_id == $heading->id) : ?> selected <?php endif; ?>><?= $heading->name ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                        <em id="tagHeading"></em>
                     </div>
                     <div class="d-grid">
                         <button class="btn btn-primary" id="btnTag"><?php if (isset($_GET['id'])) : ?> Update <?php else : ?> Save <?php endif; ?></button>
