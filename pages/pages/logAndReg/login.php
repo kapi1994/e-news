afba3e9d0ec37917c6dc97cc80d3aeb38b2be134
kapi1994 <nemanjajovicic13@gmail.com>
2021-12-23 23:18:11 +0100

    Displaying message if account is recovered

--- pages/pages/logAndReg/login.php
+++ pages/pages/logAndReg/login.php
@@ -7,6 +7,17 @@ if (isset($_SESSION['user'])) {
     <div class="container">
         <div class="row my-5">
             <div class="col-lg-6 mx-auto">
+                <?php
+                if (isset($_SESSION['activateAcc'])) :
+                ?>
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <strong>Your account is reactivated</strong>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                <?php
+                    unset($_SESSION['activateAcc']);
+                endif; ?>
+
                 <ul class="list-group">
                     <?php
 
