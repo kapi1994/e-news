b31e62c86b373daa6dc844465ea00c61b04e84a5
kapi1994 <nemanjajovicic13@gmail.com>
2021-12-24 01:45:20 +0100

    added functinality to be disibaled, if user 3 times unsuccessfull attempts

--- models/action/login.php
+++ models/action/login.php
@@ -5,38 +5,48 @@ if (isset($_POST['btnSubmit'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $rePassword = '/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/';
+
     $errors = [];
 
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         array_push($errors, "Your email isn't ok");
     }
-    if (!preg_match($rePassword, $password)) {
-        array_push($errors, "Your password isn't ok");
-    }
-    if (count($errors) > 0) {
 
+    if (count($errors) > 0) {
         $_SESSION['errors'] = $errors;
         header("Location:../../index.php?page=login");
     } else {
         require_once '../../config/connection.php';
         require_once '../function.php';
         try {
-            $user = loginUser($email, $password);
-            if ($user) {
-                $userRole = $user->roleName;
-                $_SESSION['user'] = $user;
-                if ($userRole == "User") {
-                    header("Location: ../../index.php");
-                } else if ($userRole == "Urednik") {
-                    header("Location:../../admin.php");
-                } else {
-                    header("Location:../../admin.php");
-                }
-                insertActivity('Login', $email);
+            if (accountIsDisabled($email)) {
+                header("Location: ../../index.php?page=login");
+                $_SESSION['errors'] = "Your account is dissabled, please check your email!";
             } else {
-                $_SESSION['errors'] = "Credentials aren't ok!";
-                header("Location:../../index.php?page=login");
+                $userQuery = "SELECT u.*, r.name as roleName FROM users u JOIN roles r ON u.role_id = r.id WHERE email=? AND password=?";
+                $userData = $conn->prepare($userQuery);
+                $passwordMD = md5($password);
+                $userData->execute([$email, $passwordMD]);
+                if ($userData->rowCount() == 0) {
+                    insertActivity("Invalid password", $email);
+                    if (!insertThreeTimesInFive($email)) {
+                        $_SESSION['errors'] = "Incorect password";
+                    } else {
+                        insertActivity('Dissabled account', $email);
+                        $_SESSION['errors'] = "You have been locket out of account, an email has been sent
+                        to your email to reset your password";
+                    }
+                    header("Location: ../../index.php?page=login");
+                } else {
+                    $user = $userData->fetch();
+                    // var_dump($user);
+                    $_SESSION['user'] = $user;
+                    if ($user->roleName == "User") {
+                        header("Location:../../index.php");
+                    } else {
+                        header("Location:../../admin.php");
+                    }
+                }
             }
         } catch (PDOException $th) {
             echo $th->getMessage();
