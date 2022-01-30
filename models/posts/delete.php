3b7fcc9363f94fa6db0915cdcb5afc058a0b8e10
kapi1994 <nemanjajovicic13@gmail.com>
2022-01-30 12:32:11 +0100

    Addad funcionality to delete posts

--- models/posts/delete.php
+++ models/posts/delete.php
@@ -1,2 +1,16 @@
 <?php
 header("Content-type:application/json");
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    require_once '../../config/connection.php';
+    require_once '../function.php';
+    try {
+        deleteData('posts', 'id', $_POST['id']);
+        http_response_code(204);
+    } catch (PDOException $th) {
+        echo json_encode("This data can't be delete");
+        http_response_code(500);
+    }
+} else {
+    http_response_code(404);
+}
