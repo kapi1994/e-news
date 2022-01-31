2a561f27c55fa9d824d91c541ef2817d7c5fdfe3
kapi1994 <nemanjajovicic13@gmail.com>
2022-01-31 17:33:38 +0100

    Replaced response error message

--- models/tags/delete.php
+++ models/tags/delete.php
@@ -1,13 +1,14 @@
 <?php
 header("Content-type:application/json");
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    require_once "../../config/connection.php";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    require_once '../../config/connection.php';
     require_once '../function.php';
     try {
-        deleteData("tags", 'id', $_POST['id']);
+        deleteData('tags', 'id', $_POST['id']);
         http_response_code(204);
     } catch (PDOException $th) {
-        echo json_encode($th->getMessage());
+        echo json_encode("This data can't be delete");
         http_response_code(500);
     }
 } else {
