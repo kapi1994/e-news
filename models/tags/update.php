4b4e28790ada24fd1e28427d7e0d60fd3d98f548
kapi1994 <nemanjajovicic13@gmail.com>
2022-02-07 18:29:16 +0100

    Update and add or remove tags in relation table

--- models/tags/update.php
+++ models/tags/update.php
@@ -2,10 +2,14 @@
 header("Content-type:application/json");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $reName = '/^([A-Z]{1,}|[A-Z][a-z]{2,15}|[\w\d]{1,})(\s[A-Z]{1,}|\s[A-Z][a-z]{2,15}|\s[a-z]{2,})*$/';
-    $heading = $_POST['heading_id'];
+    $headings_arr = $_POST['headings'];
+    // echo json_encode($headings_arr);
     $errors = [];
     if (!preg_match($reName, $_POST['id'])) {
         array_push($errors, "Tag name isn't ok");
+    }
+    if (count($headings_arr) == 0) {
+        array_push($errors, "You must choose at least one heading");
     }
     if (count($errors) > 0) {
         foreach ($errors as $error) {
@@ -24,7 +28,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 http_response_code(409);
             } else {
                 try {
-                    updateTag($_POST['name'], $date, $heading, $_POST['id']);
+                    $conn->beginTransaction();
+                    updateTag($_POST['name'], $date, $headings_arr, $_POST['id']);
+                    $conn->commit();
                     http_response_code(204);
                 } catch (PDOException $th) {
                     echo json_encode($th->getMessage());
@@ -33,7 +39,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
         } else {
             try {
-                updateTag($_POST['name'], $date, $heading, $_POST['id']);
+                $conn->beginTransaction();
+                updateTag($_POST['name'], $date, $headings_arr, $_POST['id']);
+                $conn->commit();
                 http_response_code(204);
             } catch (PDOException $th) {
                 echo json_encode($th->getMessage());
