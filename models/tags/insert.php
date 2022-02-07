07e6b663fe120e799cc2ac0c6a303a935aa15daa
kapi1994 <nemanjajovicic13@gmail.com>
2022-02-07 18:28:16 +0100

    Added transaction, insert in relation table

--- models/tags/insert.php
+++ models/tags/insert.php
@@ -4,9 +4,13 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $name = $_POST['name'];
     $reName = '/^([A-Z]{1,}|[A-Z][a-z]{2,15}|[\w\d]{1,})([\.\:])?(\s[A-Z]{1,}|\s[A-Z][a-z]{2,15}|\s[a-z]{2,})*$/';
     $errors = [];
-    $heading = $_POST['heading_id'];
+    $headings_arr = $_POST['headings'];
+
     if (!preg_match($reName, $name)) {
         array_push($errors, "Name of the tag isn't ok");
+    }
+    if (count($headings_arr) == 0) {
+        array_push($errors, "You must choose at least one heading");
     }
     if (count($errors) > 0) {
         foreach ($errors as $error) {
@@ -22,10 +26,13 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             echo json_encode("Name of the tag is already taken");
         } else {
             try {
-                insertTag($name, $heading);
+                $conn->beginTransaction();
+                insertTag($name, $headings_arr);
+                $conn->commit();
                 echo json_encode("Tag is successfully created");
                 http_response_code(201);
             } catch (PDOException $th) {
+                $conn->rollBack();
                 echo json_encode($th->getMessage());
                 http_response_code(500);
             }
