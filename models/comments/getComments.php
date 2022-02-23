b9f8cc40ce939fc4bb0fdaecb34b283e60ee3137
kapi1994 <nemanjajovicic13@gmail.com>
2022-02-23 23:16:51 +0100

    added new params

--- models/comments/getComments.php
+++ models/comments/getComments.php
@@ -3,11 +3,16 @@ session_start();
 header("Content-type:application/json");
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
     $post = $_GET['post'];
-    $parent_comment = isset($_GET['comment']) ? $_GET['comment'] : 0;
+    $parent_comment = $_GET['comment'];
     require_once '../../config/connection.php';
     require_once '../function.php';
-    $user = isset($_SESSION['user']) ? $_SESSION['user'] : '';
-    $comments = getCommentsWithReaction($post, $user->id, $parent_comment);
+
+
+
+    $user = isset($_SESSION['user']) ? $_SESSION['user']->id : '';
+    $comments = getCommentsWithReaction($post, $user, $parent_comment);
+
+
     echo json_encode([
         'comments' => $comments,
         'user' => $user
